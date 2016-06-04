0e5cb2ed4d805857dc344d1c2c4300469463b087
Gusti <augustincalin@yahoo.com>
2016-06-04 20:41:06 +0200

    Adds less, image loader
    Before creating the vs project.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ require('angular-aria');
 require('angular-messages');
 require('angular-material');
 
+require('style!css!../node_modules/angular-material/angular-material.css');
+require('../styles/app.less');
+
 var app = angular.module('euro2016', ['ngMaterial', 'ngMessages']);
 app.run(function ($log) {
     $log.debug('app started...');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-    entry:['./js/index.js'],
-    output:{
-        path:'public/js/',
-        filename:'bundle.js'
-    }
+    entry: ['./js/index.js'],
+    output: {
+        path: 'public/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.(jpg|png)$/,
+                exclude:/node_modules/,
+                loader: "url-loader?limit=10000"
+            },
+            {
+				test: /\.less$/,
+				exclude: /node_modules/,
+				loader: "style-loader!css-loader!less-loader"
+			}
+
+        ]
+    },
 }
